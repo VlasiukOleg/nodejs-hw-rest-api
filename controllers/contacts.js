0e890a8c72a090cf73c319e4bb33771f14e6621e
Oleg <ikslprint@gmail.com>
2023-08-08 19:36:21 +0300

    add pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -19,7 +19,9 @@ const addSchema = Joi.object({
  const getAll =  async (req, res, next) => {
         try {
         const {_id: owner} = req.user;
-        const allContacts = await Contact.find({owner}, '-createdAt -updatedAt').populate('owner', 'email');
+        const {page = 1, limit = 20} = req.query;
+        const skip = (page-1) * limit; 
+        const allContacts = await Contact.find({owner}, '-createdAt -updatedAt', {skip,limit}).populate('owner', 'email');
         res.json(allContacts);
         } catch (error) {
           next(error);
